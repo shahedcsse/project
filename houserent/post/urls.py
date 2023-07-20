7bcfe4069874e4a43cb9917405fd776b3d9f9f8e
shahed hasan <75186167+shahedcsse@users.noreply.github.com>
2023-07-20 05:05:45 -0700

    delete

--- houserent/post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('',views.index, name='posts'),
-    path('post',views.post, name='post'),
-    path('search',views.search, name='search'),
-]
